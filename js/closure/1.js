ca72200eb7ff03ca1d61a3dc9625d683dc9b48ba
LeeAt67 <1036232425@qq.com>
2025-06-13 17:12:44 +0800

    由类数组对象arguments -> 函数柯里化

--- js/closure/1.js
+++ js/closure/1.js
@@ -5,7 +5,22 @@ function add(a, b, c) {
 }
 
 add(1, 2, 3)
-
+function curry(fn) {
+    // fn? 参数 最终要执行的功能， 闭包中的自由变量 词法定义的环境
+    // curry 函数 包装fn， 慢慢收集参数
+    // ...args 所有的参数 自由变量
+    let judge = (...args) => {
+        // es6 rest 运算符
+        // 任何地方都可以访问到定义时候的fn
+        if (args.length === fn.length) {
+            // 退出条件
+            return fn(...args)
+        }
+        return (...newArgs) => judge(...args, ...newArgs);
+    }
+    return judge;
+}
+// 柯里化  手写curry 函数
 let addCurry = curry(add)
-
-addCurry(1)(2)(3)
+// 逐步获取函数需要的参数， 当到达fn需要的参数数量时， 执行结果。
+console.log(addCurry(1)(2)(3));
