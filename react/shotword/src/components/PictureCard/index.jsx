5c85ed3397654c4e30fdc4aeba2740f0dc51089b
LeeAt67 <1036232425@qq.com>
2025-07-04 21:20:50 +0800

    为PictureCard组件添加图片上传功能，使用FileReader预览上传的图片，并更新状态以显示预览图像。

--- react/shotword/src/components/PictureCard/index.jsx
+++ react/shotword/src/components/PictureCard/index.jsx
@@ -1,3 +1,6 @@
+import {
+    useState,
+} from 'react'
 import './style.css'
 
 const PictureCard = (props) => {
@@ -6,10 +9,37 @@ const PictureCard = (props) => {
         word,
         uploadImg,
     } = props
-    return (
 
-        <div>
-            PictureCard
+    const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png')
+    const uploadImgData = (e) => {
+        // 可选链操作符
+        const file = (e.target).files?.[0];
+        if (!file) { return; }
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const data = reader.result;
+                setImgPreview(data);
+                uploadImg(data);
+                resolve(data);
+            }
+            reader.onerror = (error) => { reject(error); };
+        })
+    }
+
+    return (
+        <div className="card">
+            <input
+                type="file"
+                id="selectImage"
+                accept=".jpg,.jpeg,.png,.gif"
+                onChange={uploadImgData}
+            />
+            <label htmlFor="selectImage" className="upload">
+                <img src={imgPreview} alt="preview" ></img>
+            </label>
+            <div className="word">{word}</div>
         </div>
     )
 }
