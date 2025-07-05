012c7f4347d42ae257122928ff944fe92e24d45d
LeeAt67 <1036232425@qq.com>
2025-07-05 17:22:07 +0800

    在PictureCard组件中添加音频播放功能，用户可以点击图标播放相关音频。

--- react/shotword/src/components/PictureCard/index.jsx
+++ react/shotword/src/components/PictureCard/index.jsx
@@ -27,6 +27,10 @@ const PictureCard = (props) => {
             reader.onerror = (error) => { reject(error); };
         })
     }
+    const playAudio = () => {
+        const audioEle = new Audio(audio);
+        audioEle.play();
+    }
 
     return (
         <div className="card">
@@ -40,6 +44,12 @@ const PictureCard = (props) => {
                 <img src={imgPreview} alt="preview" ></img>
             </label>
             <div className="word">{word}</div>
+            {audio && (
+                <div className="playAudio" onClick={playAudio}>
+                    <img width="20px" src="https://res.bearbobo.com/resource/upload/Omq2HFs8/playA-3iob5qyckpa.png" alt="logo" />
+                </div>
+            )}
+
         </div>
     )
 }
