ce2abe8ff4353e7473ba9aa77429752a9f7e3f51
LeeAt67 <1036232425@qq.com>
2025-07-09 11:38:52 +0800

    更新TodoList组件，新增props以接收待办事项列表，优化渲染逻辑，添加无待办事项时的提示信息。

--- react/hooks-todos/src/components/Todos/TodoList.jsx
+++ react/hooks-todos/src/components/Todos/TodoList.jsx
@@ -1,5 +1,16 @@
 import TodoItem from "./TodoItem";
-const TodoList = () => {
-  return <>TodoList</>;
+const TodoList = (props) => {
+  const { todos } = props;
+  return (
+    <>
+      {/*TodoList*/}
+      {todos.length > 0 ? (
+        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+      ) : (
+        <p>暂无待办事项</p>
+      )}
+      <TodoItem />
+    </>
+  );
 };
 export default TodoList;
