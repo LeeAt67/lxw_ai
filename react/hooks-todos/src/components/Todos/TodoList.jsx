e4f29b8bcf7e6d34831efdb79ab802f7feb1e1bc
LeeAt67 <1036232425@qq.com>
2025-07-10 11:21:05 +0800

    更新TodoList组件，新增切换和删除待办事项的功能，优化组件结构，提升用户交互体验。

--- react/hooks-todos/src/components/Todos/TodoList.jsx
+++ react/hooks-todos/src/components/Todos/TodoList.jsx
@@ -1,16 +1,22 @@
 import TodoItem from "./TodoItem";
 const TodoList = (props) => {
-  const { todos } = props;
+  const { todos, onToggle, onDelete } = props;
   return (
-    <>
+    <ul className="todo-list">
       {/*TodoList*/}
       {todos.length > 0 ? (
-        todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={() => onToggle(todo.id)}
+            onDelete={() => onDelete(todo.id)}
+          />
+        ))
       ) : (
         <p>暂无待办事项</p>
       )}
-      <TodoItem />
-    </>
+    </ul>
   );
 };
 export default TodoList;
