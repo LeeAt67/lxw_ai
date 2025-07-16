8799f6f5e4f0317fdec04bf91bcd48219bb76b7e
LeeAt67 <1036232425@qq.com>
2025-07-16 19:45:52 +0800

    新增useTodos自定义Hook，封装待办事项的增删改查逻辑，提升代码复用性和可维护性。

---
+++ react/reducer-context/src/hooks/useTodos.js
@@ -0,0 +1,19 @@
+import { useReducer } from "react";
+import todoReducer from "../reducers/todoReducer";
+// es6 参数的默认值
+// {useTodos, } key:valule 省略
+// `` 模板字符串
+// 解构 [] = [] {} = {}
+// 展开运算符， ... rest 运算符
+export function useTodos(initial = []) {
+  const [todos, dispatch] = useReducer(todoReducer, initial);
+  const addTodo = (text) => dispatch({ type: "ADD_TODO", text });
+  const toggleTodo = (id) => dispatch({ type: "TOGGLE_TODO", id });
+  const removeTodo = (id) => dispatch({ type: "REMOVE_TODO", id });
+  return {
+    todos,
+    addTodo,
+    toggleTodo,
+    removeTodo,
+  };
+}
