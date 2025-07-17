0aea6df43fa64ac5dc54200359b3f5c64aed1594
LeeAt67 <1036232425@qq.com>
2025-07-17 18:05:01 +0800

    更新main.jsx文件，移除StrictMode，新增全局状态管理和路由支持，提升应用结构的灵活性和可维护性。

--- react/react-repos/src/main.jsx
+++ react/react-repos/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// 页面级别组件由路由驱动
+createRoot(document.getElementById("root")).render(
+  <GlobalProvider>
+    <Router>
+      <App />
+    </Router>
+  </GlobalProvider>
+);
