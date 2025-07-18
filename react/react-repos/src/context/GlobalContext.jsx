7a52a6f16cbc7d1920b65ef5c5b50b59fcbc9c47
LeeAt67 <1036232425@qq.com>
2025-07-18 19:15:09 +0800

    在GlobalContext.jsx中引入useReducer和repoReducer，新增初始状态管理，优化全局状态提供器的实现。

--- react/react-repos/src/context/GlobalContext.jsx
+++ react/react-repos/src/context/GlobalContext.jsx
@@ -1,5 +1,19 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { repoReducer } from "@/reducers/repoReducer";
+
 export const GlobalContext = createContext();
+
+const initalState = {
+  repos: [],
+  loading: false,
+  error: null,
+};
 export const GlobalProvider = ({ children }) => {
-  return <GlobalContext.Provider value="">{children}</GlobalContext.Provider>;
+  const [state, dispatch] = useReducer(repoReducer, initalState);
+  return (
+    // state 应用状态
+    <GlobalContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
