4240c19efff4a4a87cef1ba931b7fe09047e796e
LeeAt67 <1036232425@qq.com>
2025-07-18 19:15:30 +0800

    新增RepoList组件，整合仓库数据展示逻辑，优化页面导航和状态处理。

--- react/react-repos/src/pages/RepoList/index.jsx
+++ react/react-repos/src/pages/RepoList/index.jsx
@@ -1,4 +1,37 @@
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useRepos } from "@/hooks/useRepos";
+
 const RepoList = () => {
-  return <>RepoList</>;
+  const { id } = useParams();
+  console.log(useParams());
+  const navigate = useNavigate();
+  // hooks
+  const { repos, loading, err } = useRepos(id);
+  console.log(repos, loading, err);
+
+  useEffect(() => {
+    if (!id.trim()) {
+      navigate("/");
+      return;
+    }
+  }, []);
+  if (loading) return <>Loading...</>;
+  if (err) return <>Error: {err}</>;
+
+  return (
+    <>
+      <h2> Repositories for {id}</h2>
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link key={repo.id} to={`/users/${id}/repos/${repo.id}`}>
+              {repo.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default RepoList;
