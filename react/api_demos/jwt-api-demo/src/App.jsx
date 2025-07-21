15a773e59dd6ef27db29c2e0ae86978fd9ae31dd
LeeAt67 <1036232425@qq.com>
2025-07-21 21:14:22 +0800

    新增mock测试文件test.js，定义了两个API接口的模拟响应，分别用于获取待办事项和仓库信息。

--- react/api_demos/jwt-api-demo/src/App.jsx
+++ react/api_demos/jwt-api-demo/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { getTodos, getRepos } from "@/api";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      //  const todosResult = await getTodos();
+      //  console.log(todosResult);
+      //  setTodos(todosResult.data.data);
 
-  return
-   (
+      const reposResult = await getRepos();
+      console.log(reposResult);
+      setRepos(reposResult.data);
+    };
+    fetchData();
+  }, []);
+  return (
     <>
-
+      {console.log("repos:", repos)}
+      <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            {repo.name} {repo.description}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
