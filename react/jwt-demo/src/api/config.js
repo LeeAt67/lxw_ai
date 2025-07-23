bec8db244685a2dc9741813c7a4efe5958e4cde6
LeeAt67 <1036232425@qq.com>
2025-07-23 16:59:30 +0800

    在axios配置文件中优化请求拦截器，调整token处理逻辑，确保只有在存在token时才添加Authorization头，提升了代码的安全性和可读性。

--- react/jwt-demo/src/api/config.js
+++ react/jwt-demo/src/api/config.js
@@ -2,9 +2,12 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:5173/api";
 // 拦截器
 axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token") || "";
   //console.log("/////");
-  let token = localStorage.getItem("token") || "";
-  config.headers.Authorization = token;
+  // let token = localStorage.getItem("token") || "";
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config;
 });
 
