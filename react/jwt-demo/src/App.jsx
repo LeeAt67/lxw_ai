043dc58fae43648fffd6f15929cd1c30353383b4
LeeAt67 <1036232425@qq.com>
2025-07-23 16:59:20 +0800

    重构App组件，新增路由功能和懒加载，优化了组件结构和用户导航体验。

--- react/jwt-demo/src/App.jsx
+++ react/jwt-demo/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState, useEffect } from "react";
-import { getUser } from "./api/user";
+import { useState, useEffect, lazy, Suspense } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+const Home = lazy(() => import("./view/Home"));
+const Login = lazy(() => import("./view/Login"));
+const Pay = lazy(() => import("./view/Pay"));
+const RequireAuth = lazy(() => import("./components/RequireAuth"));
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      const res = await getUser();
-      console.log(res);
-    })();
-  }, []);
-  return <></>;
+  useEffect(() => {}, []);
+  return (
+    <>
+      <Suspense>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/pay"
+            element={
+              <RequireAuth>
+                <Pay />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
