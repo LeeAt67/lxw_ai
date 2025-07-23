9f772582d6d70918e27303b3a34eaa178a0022ab
LeeAt67 <1036232425@qq.com>
2025-07-23 16:59:14 +0800

    在login.js中优化JWT认证逻辑，新增用户数据结构，调整token解析方式，提升代码的安全性和可读性。

--- react/jwt-demo/mock/login.js
+++ react/jwt-demo/mock/login.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 const { sign } = jwt;
 // 安全性 编码的时候加密
 // 解码的时候用于解密
+// 加盐
 const secret = "!&1244jajf";
 // login 模块 mock
 export default [
@@ -33,8 +34,10 @@ export default [
       // 生成token 颁发令牌
       return {
         token,
-        username,
-        password,
+        data: {
+          id: "001",
+          username: "admin",
+        },
       };
     },
   },
@@ -43,7 +46,9 @@ export default [
     method: "get",
     response: (req, res) => {
       // 用户端 token headers
-      const token = req.headers["authorization"];
+      const token = req.headers["authorization"].split(" ")[1];
+      console.log(token);
+
       try {
         const decode = jwt.decode(token, secret);
         console.log(decode);
